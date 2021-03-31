be30ec994d0660ea89f6a32bf7408daea850db3e
massimomochi <80513495+massimomochi@users.noreply.github.com>
2021-03-31 13:03:46 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,9 @@
+# Time series analysis
+# Greenland increase of temperature
+# Data and code from EManuela Cosma
+
+install.packages("raster")
+
+library(raster)
+
+setwd("C:/lab/greenland") # Windows
