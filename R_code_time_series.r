3cf9727907f197f06a6fb93ba5c52824a7475c1e
massimomochi <80513495+massimomochi@users.noreply.github.com>
2021-04-09 11:01:21 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -7,3 +7,49 @@ install.packages("raster")
 library(raster)
 
 setwd("C:/lab/greenland") # Windows
+
+library(raster)
+
+
+lezione precedente
+
+
+install.packages("rasterVis")
+
+library(rasterVis)
+
+rlist <- list.files(pattern="lst")
+rlist
+
+ 
+
+import <- lapply(rlist,raster)
+import
+
+ 
+
+TGr <- stack(import)
+
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr,col.regions=cl,main="Summer land surface temperature",names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015")) #
+levelplot(TGr,col.regions=cl, main="LST variation in time",
+          names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+meltlist <- list.files(pattern="melt")
+meltlist
+
+ 
+melt_import <- lapply(meltlist,raster)
+
+
+melt <- stack(import)
+melt
+
+levelplot(melt)
+
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt #sovrapposizione immagini
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+levelplot(melt_amount, col.regions=clb)
