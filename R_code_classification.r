5fba4cb3171fe4245d95d13d71acab6f0e6535fe
massimomochi <80513495+massimomochi@users.noreply.github.com>
2021-04-21 12:48:24 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,4 +1,7 @@
+#classificazione
+
 library(raster)
+library(RStoolbox)
 
 setwd("C:/lab/") # Windows
 
@@ -6,3 +9,24 @@ so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 
 # visualize RGB levels
 plotRGB(so, 1, 2, 3, stretch="lin")
+
+# Unsupervised classification
+soc <- unsuperClass(so, nClasses=3)
+cl <- colorRampPalette(c('yellow','red','black'))(100)
+plot(soc$map,col=cl)
+
+
+# Unupervised Classification with 20 classes
+
+soc20 <- unsuperClass(so, nClasses=20)
+plot(soc20$map)
+
+# Download an image from:
+# https://www.esa.int/ESA_Multimedia/Missions/Solar_Orbiter/(result_type)/images
+sun <- brick("Solar_Orbiter_s_first_view_of_the_Sun.png")
+
+ 
+
+# Unsupervised classification
+sunc <- unsuperClass(sun, nClasses=3)
+plot(sunc$map)
