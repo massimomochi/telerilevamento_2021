c46bcf5a5d9d567fed5b799ca4a006a037175ea0
massimomochi <80513495+massimomochi@users.noreply.github.com>
2021-04-28 12:47:47 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -11,8 +11,28 @@ plot(p224r63_2011)
 
 p224r63_2011
 
+
+plot(p224r63_2011$B1_sre, p224r63_2011$B1_sre, col="red", pch=19, cex=2)
 plot(p224r63_2011$B1_sre, p224r63_2011$B2_sre, col="red", pch=19, cex=2)
+
 
 pairs(p224r63_2011)
 
+# aggregate cells: resampling (ricampionamento)
+
+p224r63_2011_res <- aggregate(p224r63_2011, fact=10, fun=mean)
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=4, g=3, B=2, stretch="lin")
+plotRGB(p224r63_2011_res, r=4, g=3, B=2, stretch="lin")
+
+#rasterPCA
+
+p224r63_2011_pca <- rasterPCA(p224r63_2011_res)
+
+summary(p224r63_2011_pca$model)
+
+plot(p224r63_2011_pca$map)
+
+plotRGB(p224r63_2011_pca$map, r=1, g=2, b=3, stretch="lin")
 
