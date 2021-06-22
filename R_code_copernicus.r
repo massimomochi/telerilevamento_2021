96633536ce6932415c436273fd34f54a7ffb81e7
massimomochi <80513495+massimomochi@users.noreply.github.com>
2021-06-22 11:36:31 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -7,7 +7,7 @@ library(ncdf4)
 
 setwd("C:/lab/") # Windows
 
-albedo <- raster("")
+albedo <- raster("c_gls_ALBH_202006130000_GLOBE_PROBAV_V1.5.1.nc") # mi sono perso il passaggio per scaricare questo file, ho solo copiato il nome
 
 cl <- colorRampPalette(c('light blue','green','red','yellow'))(100) 
 plot(albedo, col=cl)
