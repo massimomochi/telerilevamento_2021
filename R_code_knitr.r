5bf9388127a7ecfab6fc116621d43fbff0b74267
massimomochi <80513495+massimomochi@users.noreply.github.com>
2021-06-23 11:46:16 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,3 +1,5 @@
+#R_code_knitr.r
+
 #utilizzo knitr
 
 install.packages("knitr")
@@ -6,7 +8,9 @@ library(knitr)
 setwd("C:/lab/") # Windows
 
 
-#per creare report PDF del codice con grafici
+#per creare report PDF del codice compresi grafici e immagini
 stitch("R_code_Greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 stitch("codice.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
+#ho riscontrato errori che non sono riuscito a risolvere come altri della classe
+#abbiamo utilizzato overleaf per creare il pdf
