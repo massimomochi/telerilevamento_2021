c050c41b3138eae3cdd0560358cb0a7ac5b0ec6f
massimomochi <80513495+massimomochi@users.noreply.github.com>
2021-06-26 18:16:00 +0200

    Update 9R_code_variability.r

--- 9R_code_variability.r
+++ 9R_code_variability.r
@@ -5,7 +5,7 @@ library(RStoolbox)
 # install.packages("RStoolbox")
 library(ggplot2) # for ggplot plotting
 library(gridExtra) # for plotting ggplots together
-# install.packages("viridis")
+install.packages("viridis")
 library(viridis) # for ggplot colouring
 
 
